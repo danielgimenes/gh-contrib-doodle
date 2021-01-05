f8de94dbf9bd2528c98852cd10b130588a8bd1d2
Daniel Gimenes <danielcgimenes@gmail.com>
2021-01-05 10:26:32 -0300

    Gem icon test

---
+++ gem.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+./mkcomms.sh "2014-05-05"
+./mkcomms.sh "2014-05-06"
+./mkcomms.sh "2014-05-11"
+./mkcomms.sh "2014-05-14"
+./mkcomms.sh "2014-05-19"
+./mkcomms.sh "2014-05-20"
+
+./mkcomms.sh "2014-05-12"
+./mkcomms.sh "2014-05-12"
+./mkcomms.sh "2014-05-12"
+./mkcomms.sh "2014-05-13"
+./mkcomms.sh "2014-05-13"

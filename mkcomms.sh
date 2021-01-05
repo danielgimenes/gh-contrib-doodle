fd6f01ad4c548679df1b7ee8c86c86a1c01aaba3
Daniel Gimenes <danielcgimenes@gmail.com>
2021-01-05 10:43:12 -0300

    Specify number of commits you want

--- mkcomms.sh
+++ mkcomms.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
 TGT=$1
+TIMES=$2
+if [ -z "$TIMES" ]
+then
+  TIMES=1
+fi
 
-echo $RANDOM > fakefile
 
-git add fakefile
+while [ $TIMES -gt 0 ]
+do
+  echo $RANDOM > fakefile
 
-git commit --date $TGT -m "target: $TGT"
+  git add fakefile
+
+  git commit --date $TGT -m "target: $TGT"
+
+  TIMES=`expr $TIMES - 1`
+done
+
+
 
 

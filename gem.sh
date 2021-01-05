48e34ffc2a30f88e41f8e39a8c2cba8325b848a6
Daniel Gimenes <danielcgimenes@gmail.com>
2021-01-05 10:46:31 -0300

    Improving Gem to use num of commits

--- gem.sh
+++ gem.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-./mkcomms.sh "2014-05-05"
-./mkcomms.sh "2014-05-06"
-./mkcomms.sh "2014-05-11"
-./mkcomms.sh "2014-05-14"
-./mkcomms.sh "2014-05-19"
-./mkcomms.sh "2014-05-20"
+WEAK=1
+MEDIUM=4
+STRONG=10
 
-./mkcomms.sh "2014-05-12"
-./mkcomms.sh "2014-05-12"
-./mkcomms.sh "2014-05-12"
-./mkcomms.sh "2014-05-13"
-./mkcomms.sh "2014-05-13"
+./mkcomms.sh "2014-05-05" $WEAK
+./mkcomms.sh "2014-05-06" $WEAK
+./mkcomms.sh "2014-05-11" $WEAK
+./mkcomms.sh "2014-05-14" $WEAK
+./mkcomms.sh "2014-05-19" $WEAK
+./mkcomms.sh "2014-05-20" $WEAK
+
+./mkcomms.sh "2014-05-12" $STRONG
+./mkcomms.sh "2014-05-13" $MEDIUM
